0983a4f161f4935aca1a96d2653072795ad4ecf2
nikmcphail <nikolasstevenmcphail@gmail.com>
2024-09-18 13:07:12 -0400

    feat: better error handling for registration page

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { register } from '@/app/action/register';
 
 export default function Register() {
-  const [error, setError] = useState("");
+  const [errors, setErrors] = useState("");
   const router = useRouter();
   const ref = useRef(null);
 
@@ -18,8 +18,11 @@ export default function Register() {
     });
 
     ref.current?.reset();
-    if (reg?.error) { 
-      setError(reg.error);
+    if (reg?.errors) { 
+      setErrors(reg.errors);
+      return;
+    } else if (reg?.error) {
+      setErrors({general: reg.error});
       return;
     } else {
       return router.push('/user/login');
@@ -30,25 +33,29 @@ export default function Register() {
     <div className="flex justify-center items-center pt-56">
       <form ref={ref} action={handleSubmit} className="form-background font-bold">
         <div className="flex flex-col items-center px-5 py-5 space-y-5">
-          {error && <div className="">{error}</div>}
+          {errors.general && <div className="text-red-500">{errors.general}</div>}
           <div className="flex flex-col">
             <label htmlFor="firstname">First Name</label>
             <input type="text" placeholder="John" id="firstname" name="firstname" className="form-input" required/>
+            {errors.firstname && <div className="text-red-500">{errors.firstname}</div>}
           </div>
 
           <div className="flex flex-col">
             <label htmlFor="lastname">Last Name</label>
             <input type="text" placeholder='Doe' id="lastname" name="lastname" className="form-input" required/>
+            {errors.lastname && <div className="text-red-500">{errors.lastname}</div>}
           </div>
 
           <div className="flex flex-col">
             <label htmlFor="email">Email</label>
             <input type="email" pattern="^[a-zA-Z0-9._%+\-]+@(uncc\.edu|charlotte\.edu)$" id="email" name="email" placeholder="johndoe@charlotte.edu" className="form-input" required/>
+            {errors.email && <div className="text-red-500">{errors.email}</div>}
           </div>
 
           <div className="flex flex-col">
             <label htmlFor="password">Password</label>
             <input type="password" placeholder="Password" id="password" name="password" className="form-input" required/>
+            {errors.password && <div className="text-red-500">{errors.password}</div>}
           </div>
 
           <button className="uncc-form-button p-3 text-white font-bold">Sign Up</button>
