8c24c47514b37077c778649fc11b09ca249ad1d1
TimGable <timgggable@gmail.com>
2024-09-25 20:58:22 -0400

    feat: Updated club and announcement schemas

--- app/models/announcement.js
+++ app/models/announcement.js
@@ -1,6 +1,40 @@
-import {Schema, mongoose} from 'mongoose';
+import { Schema, mongoose } from 'mongoose';
+import User from './user';
 
-const announcementSchema = new Schema({});
+const announcementSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  comments: [{
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const Announcement = mongoose.models.Announcement || mongoose.model("Announcement", announcementSchema);
 
